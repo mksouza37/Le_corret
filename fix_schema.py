0e75345381da76928fb9e7249b88da01fc1ea332
mksouza37 <markusn37@gmail.com>
2025-07-04 16:54:53 -0300

    commit melhoria 48

--- fix_schema.py
+++ fix_schema.py
@@ -1,7 +1,10 @@
+# fix_schema.py
+
 from models import db
 from app import app
+from sqlalchemy import text
 
 with app.app_context():
-    db.session.execute('ALTER TABLE "user" ALTER COLUMN password_hash TYPE VARCHAR(512);')
+    db.session.execute(text('ALTER TABLE "user" ALTER COLUMN password_hash TYPE VARCHAR(512);'))
     db.session.commit()
-    print("✅ Password hash column updated.")
+    print("✅ password_hash column size updated to 512.")
